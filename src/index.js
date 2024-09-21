165b06806e588fad2b1980bf8a99c235ebfce106
Wh1te0 <46064017+RWh1te0@users.noreply.github.com>
2024-09-21 14:32:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ryanxu.top": dockerHub,
+  "quay.ryanxu.top": "https://quay.io",
+  "gcr.ryanxu.top": "https://gcr.io",
+  "k8s-gcr.ryanxu.top": "https://k8s.gcr.io",
+  "k8s.ryanxu.top": "https://registry.k8s.io",
+  "ghcr.ryanxu.top": "https://ghcr.io",
+  "cloudsmith.ryanxu.top": "https://docker.cloudsmith.io",
+  "ecr.ryanxu.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ryanxu.top": dockerHub,
 };
 
 function routeByHosts(host) {
